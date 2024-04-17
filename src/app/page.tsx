3f25b39e154125464d626d692ae291fc3dc2414d
Andrei Mihale <48432916+andreimihale@users.noreply.github.com>
2024-04-17 14:18:57 +0300

    project initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="destructive" className="m-10 px-10">
+        Delete
+      </Button>
+    </main>
+  );
+}
