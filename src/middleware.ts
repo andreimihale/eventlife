877a3e1a2362cb1ecaac6d7c637691030da1c475
Andrei Mihale <48432916+andreimihale@users.noreply.github.com>
2024-04-17 22:26:13 +0300

    change public and ignored routes from webhook to webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
     "/api/uploadthing",
   ],
   ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
     "/api/uploadthing",
   ],
 });
